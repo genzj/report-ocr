1d60beea974007dc381cdeec8fabbad842f5aa1b
genzj <zj0512@gmail.com>
2024-06-30 21:00:12 -0700

    feat: :sparkles: support csv outputs

--- report_ocr.py
+++ report_ocr.py
@@ -1,17 +1,71 @@
+import heapq
 import logging
 import typing
+from csv import writer as csv_writer
 from contextlib import contextmanager
+from dataclasses import dataclass, field
+from enum import Enum
 from logging import basicConfig, getLogger
 from pathlib import Path
 from tempfile import TemporaryDirectory
+from typing import cast
 
 from cnocr.cn_ocr import CnOcr, OcrResult
+from numpy import ndarray
 from pdf2image import convert_from_path
 
 DEFAULT_OCR_MODEL = "densenet_lite_136-gru"
 
 
 L = getLogger("report_ocr")
+
+
+class Orientation(Enum):
+    HORIZONTAL = 1
+    VERTICAL = 2
+
+    def to_baseline_index(self) -> int:
+        """baseline of a vertical group is in the X dimensison, vice versa"""
+        return 0 if self == Orientation.VERTICAL else 1
+
+    def to_sort_index(self) -> int:
+        """elements in a vertical group are sorted by their Y position, vice versa"""
+        return 1 if self == Orientation.VERTICAL else 0
+
+    def calc_core(self, result: OcrResult) -> float:
+        position = cast(ndarray, result.position)
+        return position[:, self.to_baseline_index()].mean()
+
+    def calc_sort_weight(self, result: OcrResult) -> float:
+        position = cast(ndarray, result.position)
+        return position[:, self.to_sort_index()].mean()
+
+
+@dataclass
+class Group:
+    orientation: Orientation
+    tolerance: float = 5
+    baseline: float = 0
+    results: list[tuple[float, OcrResult]] = field(default_factory=list)
+
+    def is_same_group(self, result: OcrResult) -> bool:
+        return (
+            abs(self.baseline - self.orientation.calc_core(result))
+            <= self.tolerance
+        )
+
+    def add(self, result: OcrResult):
+        core = self.orientation.calc_core(result)
+        weight = self.orientation.calc_sort_weight(result)
+        n = len(self.results)
+        self.baseline = (self.baseline * n + core) / (n + 1)
+        heapq.heappush(self.results, (weight, result))
+
+    def try_add(self, result: OcrResult) -> bool:
+        if not self.is_same_group(result):
+            return False
+        self.add(result)
+        return True
 
 
 @contextmanager
@@ -44,27 +98,56 @@ def ocr_first_page(ocr: CnOcr, pdf: str | Path) -> list[OcrResult]:
         raise
 
 
+def ocr_all_pdfs( data_dir: str | Path = "data") -> typing.Iterator[tuple[Path, list[OcrResult]]]:
+    data_dir = Path(data_dir)
+    if not data_dir.is_dir:
+        raise ValueError("data_dir must be a dir")
+
+    ocr_obj = CnOcr(DEFAULT_OCR_MODEL)
+    for pdf in data_dir.glob("*.pdf"):
+        yield pdf, ocr_first_page(ocr_obj, pdf)
+
+
+def ensure_output_dir(output_dir: str | Path) -> Path:
+    output_dir = Path(output_dir)
+    if not output_dir.exists():
+        output_dir.mkdir(parents=True, exist_ok=True)
+    if not output_dir.is_dir:
+        raise ValueError("output must be a dir")
+    return output_dir
+    
+
 def ocr_to_txt(
     data_dir: str | Path = "data", output_dir: str | Path = "output"
 ):
     L.info("OCR all pdf in %s and save text to %s", data_dir, output_dir)
+    output_dir = ensure_output_dir(output_dir)
 
-    data_dir = Path(data_dir)
-    output_dir = Path(output_dir)
-    if not output_dir.exists():
-        output_dir.mkdir(parents=True, exist_ok=True)
-
-    if not data_dir.is_dir:
-        raise ValueError("data_dir must be a dir")
-    if not output_dir.is_dir:
-        raise ValueError("output must be a dir")
-
-    ocr_obj = CnOcr(DEFAULT_OCR_MODEL)
-    for pdf in data_dir.glob("*.pdf"):
+    for pdf, results in ocr_all_pdfs(data_dir):
         output_name = output_dir / Path(pdf.with_suffix(".txt").name)
         with output_name.open("w", encoding="utf-8") as output:
-            results = ocr_first_page(ocr_obj, pdf)
             output.writelines(str(result) + "\n" for result in results)
+
+
+def ocr_to_csv(
+    data_dir: str | Path = "data", output_dir: str | Path = "output"
+):
+    L.info("OCR all pdf in %s and save csv to %s", data_dir, output_dir)
+    output_dir = ensure_output_dir(output_dir)
+
+    for pdf, results in ocr_all_pdfs(data_dir):
+        output_name = output_dir / Path(pdf.with_suffix(".csv").name)
+        with output_name.open("w", encoding="utf-8") as output:
+            csv = csv_writer(output)
+            group = Group(Orientation.HORIZONTAL)
+            for result in results:
+                if group.try_add(result):
+                    continue
+                csv.writerow(ele.text for _, ele in sorted(group.results))
+                group = Group(Orientation.HORIZONTAL)
+                group.add(result)
+            # don't forget to write the last group
+            csv.writerow(ele.text for _, ele in sorted(group.results))
 
 
 if __name__ == "__main__":
@@ -74,3 +157,4 @@ if __name__ == "__main__":
         force=True,
     )
     ocr_to_txt()
+    ocr_to_csv()
